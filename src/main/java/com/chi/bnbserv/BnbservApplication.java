1496b63e0045f1e0a4a737c1d7d8e4df587e6d8f
natsu-chi <vvv488322@gmail.com>
2024-10-16 10:13:12 +0800

    init commit

---
+++ src/main/java/com/chi/bnbserv/BnbservApplication.java
@@ -0,0 +1,13 @@
+package com.chi.bnbserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chi/bnbserv/BnbservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chi.bnbserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnbservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
