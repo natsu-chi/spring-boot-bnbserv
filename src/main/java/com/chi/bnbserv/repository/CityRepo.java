1447e1ce8cfd87b50c86a89fe700ad4084519fad
natsu-chi <vvv488322@gmail.com>
2024-11-11 15:43:00 +0800

    Update: 頁面顯示調整

--- src/main/java/com/chi/bnbserv/repository/CityRepo.java
+++ src/main/java/com/chi/bnbserv/repository/CityRepo.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.chi.bnbserv.entity.City;
 import com.chi.bnbserv.dto.CityDetailsDto;
 
+@Repository
 public interface CityRepo extends JpaRepository<City, Integer>{
     @Query("SELECT new com.chi.bnbserv.dto.CityDetailsDto(a.id, a.name, b.name, c.name) " + 
            "FROM City a " + 
